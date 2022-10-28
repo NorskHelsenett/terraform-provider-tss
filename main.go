5f627fc59fa3c77f3788060311097aaa8c171a06
Vidar Anima Normann <vidar.normann@nhn.no>
2022-10-28 13:38:42 +0200

    Update debug syntax in mian.go

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	// Used for debugging
-
 	"flag"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -20,40 +18,12 @@ func main() {
 
 	opts := &plugin.ServeOpts{
 		Debug:        debug,
-		ProviderAddr: "registry.terraform.io/example-namespace/example",
+		ProviderAddr: "terraform.local/nhn/tss",
 		ProviderFunc: func() *schema.Provider {
 			return tss.Provider()
 		},
 	}
 
 	plugin.Serve(opts)
-
-	// var debugMode bool
-	// flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
-	// flag.Parse()
-
-	// if debugMode {
-
-	// 	// err := plugin.Debug(context.Background(), "terraform.local/nhn/tss", opts)
-	// 	// if err != nil {
-	// 	// 	log.Fatal(err.Error())
-	// 	// }
-	// 	// return
-	// 	opts := &plugin.ServeOpts{
-	// 		ProviderFunc: func() *schema.Provider {
-	// 			return tss.Provider()
-	// 		},
-	// 		Debug: true,
-	// 	}
-	// 	plugin.Serve(opts)
-	// } else {
-	// 	opts := &plugin.ServeOpts{
-	// 		ProviderFunc: func() *schema.Provider {
-	// 			return tss.Provider()
-	// 		},
-	// 	}
-	// 	plugin.Serve(opts)
-
-	// }
 
 }
